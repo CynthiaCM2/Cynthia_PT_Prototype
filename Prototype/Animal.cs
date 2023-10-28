7db7fd23a6eb3e7301db0fb586376caa93a4c327
cynth <cynth@LAPTOP-FM7HKCC1>
2023-10-27 22:09:48 -0600

    Agregar archivos de proyecto.

---
+++ Prototype/Animal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    //Se crea la clase con la función "ICloneable" que hará que se cree un método llamado Clone  
+    public class Animal : ICloneable
+    {
+        //Se crean los atributos de la función de la clase//
+        public int Patas { get; set; } 
+        public string Nombre { get; set; }
+
+        public Detalles Rasgos { get; set; }
+
+        //Método que hará que se clonen los objetos//
+        public object Clone()
+        {
+            Animal clonado = this.MemberwiseClone() as Animal;
+            Detalles detalles = new Detalles();
+            detalles.Color= this.Rasgos.Color;
+            detalles.Raza = this.Rasgos.Raza;
+            clonado.Rasgos = detalles;
+            return clonado;
+        }
+               
+    }
+
+    public class Detalles
+    {
+        public string Color { get; set; }
+        public string Raza { get; set; }
+    }
+}
---
+++ Prototype/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Se crean los objetos de los atributos//
+            Animal oAnimal = new Animal() { Nombre="Oveja Dolly", Patas=4};
+            oAnimal.Rasgos = new Detalles();
+            oAnimal.Rasgos.Color = "Blanca";
+            oAnimal.Rasgos.Raza = "Oveja";
+
+            Animal oAnimalClonado = oAnimal.Clone() as Animal;
+            oAnimalClonado.Rasgos.Color = "Negro";
+            oAnimalClonado.Nombre = "Oveja negra";
+
+            Console.WriteLine("animal original: "+oAnimal.Rasgos.Color);
+            Console.WriteLine("animal clonado: "+oAnimalClonado.Rasgos.Color);
+            Console.WriteLine("animal original: "+oAnimal.Nombre);
+            Console.WriteLine("animal clonado: "+oAnimalClonado.Nombre);
+
+        }
+    }
+}
